a81f1f25ff5d41e2e7cf7440f21699e6340fd298
johnvf <john.victor.faichney@gmail.com>
2016-02-01 23:28:09 -0800

    intro SVG icon

--- client/js/pages/Landing.js
+++ client/js/pages/Landing.js
@@ -2,7 +2,8 @@ var React = require('react');
 
 var Loader = require('react-loader');
 
-var Icon = require('../lib_components/Icon');
+// var Icon = require('../lib_components/Icon');
+var BigIcon = require('../lib_components/BigIcon');
 
 var Landing = React.createClass({
 
@@ -16,7 +17,12 @@ var Landing = React.createClass({
     return (
       <div className="row banner" >
         <a href ='/adapt'>
-          <Icon className ="landing_icon" symbolID="icon-icon_logo"/>
+          <BigIcon className ="landing_icon Adapt" symbolID="Adapt"/>
+          <BigIcon className ="landing_icon Evaluate" symbolID="Evaluate"/>
+          <BigIcon className ="landing_icon Monitor" symbolID="Monitor"/>
+          <BigIcon className ="landing_icon Implement" symbolID="Implement"/>
+          <BigIcon className ="landing_icon Design" symbolID="Design"/>
+          <BigIcon className ="landing_icon Analyze" symbolID="Analyze"/>
           <h1>ADAPT</h1>
           <h3>AN ECOSYSTEM SERVICES MODEL</h3>
         </a>
