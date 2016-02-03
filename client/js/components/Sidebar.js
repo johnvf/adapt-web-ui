16e803d1238f2804f82a00a0f228555203f45506
johnvf <john.victor.faichney@gmail.com>
2016-02-02 17:33:50 -0800

    updated navigation to match tags, style tweak

--- client/js/components/Sidebar.js
+++ client/js/components/Sidebar.js
@@ -102,7 +102,7 @@ var Sidebar = React.createClass({
               </Link>
             </li>
             <li>
-              <Link to={ "/adapt/oakland/analyze" }>
+              <Link to={ "/adapt/oakland/1_analyze" }>
                 <Icon className="green-fill" symbolID="icon-icon_map"/>
               </Link>
             </li>
