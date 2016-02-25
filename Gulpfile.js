1fa637b63ef8a6a746f5b2954abe923efcab3c39
johnvf <john.victor.faichney@gmail.com>
2016-02-24 17:28:39 -0800

    mangling seems to break Heroku - unmangling

--- Gulpfile.js
+++ Gulpfile.js
@@ -59,7 +59,7 @@ gulp.task('browserify', function() {
   }))
   .pipe(sourcemaps.init({loadMaps: true}))
   .pipe(concat('main.js'))
-  .pipe(uglify({mangle: true}))
+  .pipe(uglify({mangle: false}))
   .pipe(rename({ suffix: '.min'}))
   .pipe(sourcemaps.write('./'))
   .pipe( gulp.dest('./public/js') )
